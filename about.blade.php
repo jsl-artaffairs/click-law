2f1637e1624296ea5b464a642f9b85686b4577f5
jessanmartin <jessica@jessanmartin.com>
2015-11-19 17:30:48 -0430

    add about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default') <!--Calls for the Master page template -->
+
+@section('content') <!-- declairs the section -->
+
+        <div class="container">
+            <div class="content">
+                <div class="title">About Page</div>
+            </div>         
+        </div>
+
+@stop
